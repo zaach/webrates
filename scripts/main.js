da2a772c14f4fdeba19d08eb11ca4123686834ea
Zachary Carter <zack.carter@gmail.com>
2014-02-01 15:45:06 -0800

    submit data function

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,5 @@
-var database = new Firebase('https://sweltering-fire-6680.firebaseio.com/');
+var db = new Firebase('https://sweltering-fire-6680.firebaseio.com/rates');
+
+function submitData(data) {
+  db.push(data);
+}
