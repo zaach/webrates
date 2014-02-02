1dac98461575b5bc9e70a88cf2e268aaede1f705
Heather Arthur <fayearthur@gmail.com>
2014-02-02 14:38:50 -0800

    only show details form after entering rate

--- scripts/tweaks.js
+++ scripts/tweaks.js
@@ -6,4 +6,11 @@ function checkRadioButtons() {
 $(function() {
   checkRadioButtons();
   $("input[type=radio]").on("change", checkRadioButtons);
+
+
+  $(".form_group_2").hide();
+
+  $("#submit-rate").on("click", function() {
+    $(".form_group_2").show();
+  });
 })
