3d4c91d093ec02256195276791b4c2a766a30da4
Sean Fridman <fridman@mail.sfsu.edu>
2014-02-02 11:37:03 -0800

    removing old angular / firebase code in main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,4 @@
-;$(function() {
-
-var db = new Firebase('https://sweltering-fire-6680.firebaseio.com/rates');
-
-function submitData(data) {
-  db.push(data);
-}
-
-angular.module('webRatesApp', [])
-
-.controller('MyCtrl', function ($scope, $http) {
-  $scope.data = {};
-  $scope.submitForm = function () {
-    $http.post('/url', $scope.data);
-  }
-});
+$(function() {
 
 d3.json('/scripts/mock-data.json', function(err, data) {
 
